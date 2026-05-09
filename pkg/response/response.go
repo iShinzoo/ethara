7b48048bcf1c1197da442fdee89213d8b03818b6
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2026-05-09 18:29:12 +0530

    feat: implement standardized api response utilities

---
+++ pkg/response/response.go
@@ -0,0 +1,29 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+func Success(
+	c *gin.Context,
+	status int,
+	message string,
+	data interface{},
+) {
+
+	c.JSON(status, gin.H{
+		"success": true,
+		"message": message,
+		"data":    data,
+	})
+}
+
+func Error(
+	c *gin.Context,
+	status int,
+	message interface{},
+) {
+
+	c.JSON(status, gin.H{
+		"success": false,
+		"error":   message,
+	})
+}
