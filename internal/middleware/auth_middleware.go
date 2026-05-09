1a3cf7b136aca189f4a6997696a7d1665bafe385
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2026-05-09 16:49:15 +0530

    feat: implement jwt authentication middleware

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/iShinzoo/ethara/pkg/config"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(cfg *config.Config) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "authorization header missing",
+			})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+			return []byte(cfg.JWTSecret), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid token",
+			})
+			c.Abort()
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		c.Set("user_id", claims["user_id"])
+		c.Set("email", claims["email"])
+
+		c.Next()
+	}
+}
